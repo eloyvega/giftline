1c5adca93460de711abd8d887bea5483f0412d0d
Eloy Vega <eloy.vega@gmail.com>
2015-10-16 16:13:50 -0500

    Inicio de modelo de intercambio

--- intercambios/models.py
+++ intercambios/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Intercambio(models.Model):
+    nombre = models.CharField(max_length=255, )
+    status = models.BooleanField(default=True, )
+    descripcion = models.TextField(blank=True)
+    participantes = models.ManyToManyField(User,)
