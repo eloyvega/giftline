9dba81e19c0f9ab1462656d97b3d43f16b67e5f1
Eloy Vega <eloy.vega@gmail.com>
2015-10-16 13:01:47 -0500

    Configuración de base de datos

--- giftline/settings.py
+++ giftline/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'giftline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'giftline_db',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        # 'HOST': '192.168.59.103',
+        'PORT': '5432',
     }
 }
 
