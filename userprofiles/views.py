7b1ab42efea33cba0613ffe0f88a26f777116456
Eloy Vega <eloy.vega@gmail.com>
2015-10-23 16:25:41 -0500

    Cambio de vistas a Class based views

--- userprofiles/views.py
+++ userprofiles/views.py
@@ -3,20 +3,29 @@ from django.conf import settings
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import user_passes_test
+from django.views.generic.base import View, RedirectView
+from django.core.urlresolvers import reverse
 
-@user_passes_test(lambda user: not user.username, login_url=settings.HOME_URL, redirect_field_name=None)
-def signup(request):
-    if request.method == 'POST':
+from giftline.mixins import AnonymousRequiredMixin
+
+
+class SignupView(AnonymousRequiredMixin, View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'userprofiles/signup.html', {'form': form})
+
+    def post(self, request):
         pass
-    form = UserCreationForm()
-    return render(request, 'userprofiles/signup.html', {'form': form})
 
 
-@user_passes_test(lambda user: not user.username, login_url=settings.HOME_URL, redirect_field_name=None)
-def signin(request):
-    follow = request.GET.get('next', '/')
-    if request.method == "POST":
+class SigninView(AnonymousRequiredMixin, View):
+    def get(self, request):
+        follow = self.get_next(request)
+        form = AuthenticationForm()
+        return render(request, 'userprofiles/signin.html', {'form': form, 'redirect_to': follow})
+
+    def post(self, request):
+        follow = self.get_next(request)
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
@@ -28,10 +37,15 @@ def signin(request):
                 return HttpResponse("Inactive user.")
         else:
             return HttpResponseRedirect(settings.LOGIN_URL)
-    form = AuthenticationForm()
-    return render(request, 'userprofiles/signin.html', {'form': form, 'redirect_to': follow})
+
+    def get_next(self, request):
+        return request.GET.get('next', '/')
 
 
-def signout(request):
-    logout(request)
-    return HttpResponseRedirect('/')
+class SignoutView(RedirectView):
+    permanent = False
+    query_string = True
+
+    def get_redirect_url(self):
+        logout(self.request)
+        return reverse('index')
