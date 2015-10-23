7ebd3276ac693a2ac4e189be66e9de5f3e9001fd
Eloy Vega <eloy.vega@gmail.com>
2015-10-23 13:03:24 -0500

    Agregado de mixin de user_passes_test para anonymous required

--- giftline/mixins.py
+++ giftline/mixins.py
@@ -1,4 +1,6 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.utils.decorators import method_decorator
+from django.conf import settings
 
 
 """
@@ -9,3 +11,12 @@ class LoginRequiredMixin(object):
     def as_view(cls, **initkwargs):
         view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
         return login_required(view)
+
+
+"""
+Mixin para las vistas basadas en clases que requieren que el usuario sea anonimo
+"""
+class AnonymousRequiredMixin(object):
+    @method_decorator(user_passes_test(lambda user: not user.username, login_url=settings.HOME_URL, redirect_field_name=None))
+    def dispatch(self, *args, **kwargs):
+        return super(AnonymousRequiredMixin, self).dispatch(*args, **kwargs)
