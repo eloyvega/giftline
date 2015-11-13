19bc7efa318d99ffde09c5ae68aedf007f69b8f2
Eloy Vega <eloy.vega@gmail.com>
2015-11-13 12:43:48 -0600

    Cambio en settings

--- giftline/settings.py
+++ giftline/settings.py
@@ -22,7 +22,7 @@ env = environ.Env()
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f=&!#du_*2r7%uqm8k*j%la%_)d!5-ujdgy)4^gxqmwzuf!_uc'
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='CHANGEME!!!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', False)
